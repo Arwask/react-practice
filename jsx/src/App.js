db13523839b3bedab5acc4a907cb53585be14e0a
Arwa <arwa.it15@gmail.com>
2018-03-23 14:05:43 -0500

    make two components and render

--- jsx/src/App.js
+++ jsx/src/App.js
@@ -1,7 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// Timer component's job is to return the current time every 1000ms. The App component extends the Timer component
+class Timer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date().toLocaleTimeString()
+    }; // initial state of the app when it starts
+  }
+  componentDidMount() {
+    // you can only change the state inside a componentDidMount method. This method is called right after the component is mounted
+    setInterval(() => {
+      this.setState({ time: new Date().toLocaleTimeString() });
+    }, 1000);
+  }
+  render() {
+    return <div>Time is: {this.state.time}</div>;
+  }
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newTime: this.props.time // props is passed on from parent component along with its state.
+    };
+    // initial state of the app when it starts
+  }
   makeNameString({ firstName, lastName }) {
     return firstName + ' ' + lastName;
   }
@@ -11,10 +37,22 @@ class App extends Component {
     lastName: 'Kuterwadliwala'
   };
 
-  element = <h1> Hello, {this.makeNameString(this.user)}!</h1>; //this.user ----> because the user variable is defined inside this component
+  nextEl = React.createElement('h1', { className: 'greeting' }, 'Welcome to my React App!');
+  element = <p> Hello, {this.makeNameString(this.user)}!</p>; //this.user ----> because the user variable is defined inside this component
+  // render() {
+  //   //es6 syntax ---- instead of saying let render : () => {}, we say render() {//put the functionalities in here}
+  //   return this.nextEl;
+  // }
+
   render() {
-    //es6 syntax ---- instead of saying let render : () => {}, we say render() {//put the functionalities in here}
-    return this.element;
+    // React DOM compares the element and its children to the previous one, and only applies the DOM updates necessary to bring the DOM to the desired state. So here, only the timer will update and the rest stays same.
+    return (
+      <div>
+        <div className="yellowBg">{this.nextEl}</div>
+        <div>{this.element}</div>
+        <Timer />
+      </div>
+    );
   }
 }
 
